c5875817cbdd6f9933c426f637ee655ba2354086
luissoldevilla <luis.angelsoto@hotmail.com>
2020-03-30 14:51:09 -0400

    Wine List testing rendering on cms admin

--- src/js/cms-preview-templates/values.js
+++ src/js/cms-preview-templates/values.js
@@ -21,26 +21,39 @@ const MediaBlock = ({heading, text, imageUrl, reverse}) => {
 export default class ValuesPreview extends React.Component {
   render() {
     const {entry, getAsset} = this.props;
-    
+
     let image = getAsset(entry.getIn(["data", "image"]));
 
     // Bit of a nasty hack to make relative paths work as expected as a background image here
     if (image && !image.fileObj) {
       image = window.parent.location.protocol + "//" + window.parent.location.host + image;
     }
-    
+
     const entryValues = entry.getIn(["data", "values"]);
     const values = entryValues ? entryValues.toJS() : [];
-    
+
     return <div>
       <Jumbotron image={image} title={entry.getIn(["data", "title"])} />
+
       <div className="bg-off-white pv4">
-        <div className="mw7 center ph3 pt4">
-          {values.map(({text, heading, imageUrl}, i) =>
-            <MediaBlock key={i} text={text} heading={heading} imageUrl={imageUrl} reverse={i % 2 === 0} />
-          )}
+        <div className="ph3 mw7 center">
+          <h2 className="f2 b lh-title mb2">{entry.getIn(["data", "intro", "heading"])}</h2>
+          <p className="mb4 mw6">{entry.getIn(["data", "intro", "text"])}</p>
+
+          <div className="flex-ns mhn2-ns mb3 gridm">
+            {(entry.getIn(["data", "products"]) || []).map((product, i) => <div className="ph2-ns w-50-ns" key={i}>
+              <img src={getAsset(product.get("image"))} alt="" className="center db mb3" style={{width: "240px"}}/>
+              <p>{product.get("text")}</p>
+              <p><span class="f4">$</span>{product.get("price")}</p>
+            </div>)}
+          </div>
+
+          <div className="tc">
+            <a href="#" className="btn raise">See all products</a>
+          </div>
         </div>
       </div>
+
     </div>;
   }
 }
